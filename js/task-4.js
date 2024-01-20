4eb448f3e3c1405bf952f41c4958e8e343ae07bf
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-01-21 01:16:21 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,38 @@
-function getShippingCost(country) {
+function getShippingCost (country) {
+   const price = 120;
+  
+
+
+    switch (price ) {
     
+    case  100:
+     return `Shipping to ${country}   will cost ${price} credits`;
+    case  250:
+
+        return `Shipping to ${country}  will cost ${price}  credits`;
+    case 170:
+    return `Shipping to ${country}     will cost ${price}  credits`;
+    case 120:
+
+     return `Shipping to ${country}    will cost  ${price} credits`
+    default: 
+     return `Sorry, there is no delivery to your country`;
+    
+
+
+   }
 }
+getShippingCost("China");
+getShippingCost("Chile");
+getShippingCost("Australia");
+getShippingCost("Jamaica");
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country
+
+
+ 
